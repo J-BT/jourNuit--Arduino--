44baa8ed4e7b5e662740304523c39e68bf235adc
J-BT <jeanbaptiste.theroulde@gmail.com>
2021-01-02 19:43:44 +0100

    ajout du code + README.md

--- README.md
+++ README.md
@@ -1,7 +1,10 @@
 Jour/Nuit:
 ----------
 
-Programme sollicitant 3 pin de l'arduino
-permet d'allumer ou d'eteindre chaque LED située sur 2 des pins
-selon que l'interrupteur situé sur le pin restant soit enfoncé
-ou non 
+A l'aide de 3 PIN de l'Arduino UNO, dont 2 envoyant du courant
+ (pinMode = OUPUT) et 1 en recevant (pinMode = INPUT),
+on est capable d'allumer une LED tout en eteignant l'autre
+selon que le bouton soit enfoncé ou non.
+
+nb : Quand le Pin relié au bouton renvoit "1" cela signifie qu'il reçoit
+du courant.
--- jourNuit.ino
+++ jourNuit.ino
@@ -1,32 +1,33 @@
 const int pinNum = 2;
 const int pinLED[2] = {4, 6};
 int led;
- // 4 = blanche
- // 6 = bleue
+ // 4 = vert
+ // 6 = jaune
 
 void setup() {
   Serial.begin(9600);
   pinMode(pinNum, INPUT);
 
   for (led = 0; led<2 ; led++){
-     pinMode(led, OUTPUT);
+     pinMode(pinLED[led], OUTPUT);
   }
-  digitalWrite(pinLED[1], HIGH); // On eteint la LED 6
-  digitalWrite(pinLED[0], LOW); // On eteint la LED 4
+  digitalWrite(pinLED[0], HIGH); // On eteint la LED 1 (verte)
+  digitalWrite(pinLED[1], HIGH); // On eteint la LED 2 (jaune)
   
-
 }
 
 void loop() {
   
-  boolean a = digitalRead(pinNum);
-  Serial.println(a);
+  boolean boutonON = digitalRead(pinNum);
+  Serial.println(boutonON);  // = print 1 si bouton appuyé 
 
-  if (a){
+  if (boutonON == true){
+    digitalWrite(pinLED[0], HIGH);
     digitalWrite(pinLED[1], LOW);
   }
 
-  else if (a!=1){
+  else if (boutonON == false){
+    digitalWrite(pinLED[0], LOW);
     digitalWrite(pinLED[1], HIGH);
   }
   
